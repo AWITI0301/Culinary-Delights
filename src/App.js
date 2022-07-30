316de0a034242894f423a2dfd2f3a280bfe0f903
AWITI0301 <johnoketchawiti@gmail.com>
2022-07-30 12:48:33 +0300

    ad routes to the components and pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      
+      <Navbar />
+      <div className="container main">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/recipes" element={<Recipes />} />
+          <Route exact path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
