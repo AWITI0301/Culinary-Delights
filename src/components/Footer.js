87d03f3695636308974833e5ce810a34103f3da7
AWITI0301 <johnoketchawiti@gmail.com>
2022-07-30 12:21:50 +0300

    create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+export default function Footer(){
+    return (
+        <div className="footer container">
+            <div className="footer-section">
+                <p className="title">FoodiesHub.com</p>
+                <p>FoodiesHub is a place where you can please your soul and tummy with delicious food recepies of all cuisine. And our service is absolutely free.</p>
+                <p>&copy; 2022 | All Rights Reserved</p>
+            </div>
+            <div className="footer-section">
+                <p className="title">Contact Us</p>
+                <p>foodieshub0301@gmail.com</p>
+                <p>+254-70484-3797</p>
+                <p>00100 Nairobi City</p>
+            </div>
+            <div className="footer-section">
+                <p className="title">Socials</p>
+                <p>Facebook</p>
+                <p>Twitter</p>
+                <p>Instagram</p>
+            </div>
+        </div>
+    )
+}
