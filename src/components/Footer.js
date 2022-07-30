b6961951d402a68c1c085d5f12753c409c7aed43
AWITI0301 <johnoketchawiti@gmail.com>
2022-07-30 22:44:24 +0300

    modify links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,13 @@ export default function Footer(){
     return (
         <div className="footer container">
             <div className="footer-section">
-                <p className="title">FoodiesHub.com</p>
+                <p className="title">FarmetteHub.com</p>
                 <p>FoodiesHub is a place where you can please your soul and tummy with delicious food recepies of all cuisine. And our service is absolutely free.</p>
                 <p>&copy; 2022 | All Rights Reserved</p>
             </div>
             <div className="footer-section">
                 <p className="title">Contact Us</p>
-                <p>foodieshub0301@gmail.com</p>
+                <p>farmettehub0301@gmail.com</p>
                 <p>+254-70484-3797</p>
                 <p>00100 Nairobi City</p>
             </div>
